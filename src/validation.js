bcb6079ffc0269329ede1dd277c9c53c7373bfb4
Reza <reza1377maleki@gmail.com>
2023-01-16 12:09:49 +0330

    change iteration method inside validation function

--- src/validation.js
+++ src/validation.js
@@ -1,15 +1,15 @@
 const checkValidation = function (event) {
   let notAllowed = [
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
     "!",
     "@",
     "#",
@@ -25,15 +25,14 @@ const checkValidation = function (event) {
   ];
   let value = event.target.value;
   let is_valid = "valid";
-  notAllowed.map((symbol) => {
-    if (value.includes(`${symbol}`)) {
+  for (let symbol of notAllowed) {
+    if (value.includes(symbol)) {
       is_valid = "not-valid";
-      console.log(symbol);
       this.setState({
         valid: false,
       });
     }
-  });
+  }
   return is_valid;
 };
 
