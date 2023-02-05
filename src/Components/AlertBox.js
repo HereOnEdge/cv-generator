e76398a1469d8c96dc0d2253694ebc9ee938c8ff
Reza <reza1377maleki@gmail.com>
2023-02-06 01:03:19 +0330

    add alert-box css and all the text it needs

--- src/Components/AlertBox.js
+++ src/Components/AlertBox.js
@@ -1,6 +1,11 @@
 import React from "react";
+import "../styles/alert-box.css";
 
 class AlertBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.findAlertBoxTexts();
+  }
   findNextPage = () => {
     this.summaryPage = this.props.navLink.find({
       topic: this.props.topic,
@@ -26,18 +31,44 @@ class AlertBox extends React.Component {
       this.nextPageNode
     );
   };
+  findAlertBoxTexts = () => {
+    this.paragraph =
+      this.props.topic === "work"
+        ? "It looks like you haven't entered any work history details. We recommend you add at least one position if possible."
+        : "It looks like you haven't entered any education details.";
+    this.skipButtonTitle =
+      this.props.topic === "work"
+        ? "I don't have any work experience"
+        : "Yes, Skip anyway";
+  };
   render() {
     return (
-      <div className="alert-box">
-        <span className="close-alert"></span>
-        <span className="alert-title">More information needed</span>
-        <p className="alert-desc"></p>
-        <div className="alert-buttons">
-          <div className="alert-addButton" onClick={this.props.toggleAlertBox}>
-            Add
-          </div>
-          <div className="alert-skipButtons" onClick={this.skip}>
-            Skip
+      <div className="alert-box-background">
+        <div className="alert-box">
+          <span className="close-alert">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              onClick={this.props.toggleAlertBox}
+            >
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+            </svg>
+          </span>
+          <span className="alert-title">More information needed</span>
+          <p className="alert-desc">{this.paragraph}</p>
+          <div className="alert-buttons">
+            <div
+              className="alert-addButton"
+              onClick={this.props.toggleAlertBox}
+            >
+              Add
+            </div>
+            <div className="alert-skipButton" onClick={this.skip}>
+              {this.skipButtonTitle}
+            </div>
           </div>
         </div>
       </div>
--- src/Components/Pages/OtherMainPages.js
+++ src/Components/Pages/OtherMainPages.js
@@ -154,178 +154,181 @@ class OthersMainPage extends React.Component {
   };
   render() {
     return (
-      <div className="others-main main">
-        <div className="others-header header">
-          <div className="others-title">
-            <h1>
-              {this.props.topic === "work"
-                ? "Tell us about your work history"
-                : "Tell us about your education"}
-            </h1>
-            <p>
-              {this.props.topic === "work"
-                ? "Start with your most recent job and work backwards."
-                : "List universities, colleges or institutions where you studied. If you didn't attend education, then list your school or any other place of training. particulary if it corresponds to the position sought."}
-            </p>
+      <div className="others-main-container">
+        <div className="others-main main">
+          <div className="others-header header">
+            <div className="others-title">
+              <h1>
+                {this.props.topic === "work"
+                  ? "Tell us about your work history"
+                  : "Tell us about your education"}
+              </h1>
+              <p>
+                {this.props.topic === "work"
+                  ? "Start with your most recent job and work backwards."
+                  : "List universities, colleges or institutions where you studied. If you didn't attend education, then list your school or any other place of training. particulary if it corresponds to the position sought."}
+              </p>
+            </div>
+            <PreviewButton />
           </div>
-          <PreviewButton />
-        </div>
-        <div
-          className={`others-body body ${
-            this.props.topic === "work" ? "" : "educate-body"
-          }`}
-        >
-          <div className="others-form form">
-            <TextInput
-              className="half"
-              label={this.preReqs.first.label}
-              changeData={this.changeData}
-              field={this.preReqs.first.field}
-              placeHolder={this.preReqs.first.placeHolder}
-              validationType="text"
-              data={this.props.data}
-              topic={this.props.topic}
-              id={this.id}
-              isRed={
-                this.state[this.props.topic][this.id][
-                  this.preReqs.first.field
-                ] === "" && this.state.showVitalInputs
-                  ? true
-                  : false
-              }
-            />
-            <TextInput
-              className="half"
-              label={this.preReqs.second.label}
-              changeData={this.changeData}
-              field={this.preReqs.second.field}
-              placeHolder={this.preReqs.second.placeHolder}
-              validationType="text"
-              data={this.props.data}
-              topic={this.props.topic}
-              id={this.id}
-              isRed={
-                this.state[this.props.topic][this.id][
-                  this.preReqs.second.field
-                ] === "" && this.state.showVitalInputs
-                  ? true
-                  : false
-              }
-            />
-            <TextInput
-              className="half"
-              label={this.preReqs.third.label}
-              changeData={this.changeData}
-              field={this.preReqs.third.field}
-              placeHolder={this.preReqs.third.placeHolder}
-              validationType="text"
-              data={this.props.data}
-              topic={this.props.topic}
-              id={this.id}
-            />
-            <TextInput
-              className="half"
-              label={this.preReqs.forth.label}
-              changeData={this.changeData}
-              field={this.preReqs.forth.field}
-              placeHolder={this.preReqs.forth.placeHolder}
-              validationType="text"
-              data={this.props.data}
-              topic={this.props.topic}
-              id={this.id}
-            />
-            <DateInput
-              className="half"
-              label={this.preReqs.fifth.label}
-              changeData={this.changeData}
-              field={this.preReqs.fifth.field}
-              placeHolder={this.preReqs.fifth.placeHolder}
-              validationType="month"
-              data={this.props.data}
-              topic={this.props.topic}
-              id={this.id}
-            />
-            <DateInput
-              className="half"
-              label={this.preReqs.sixth.label}
-              changeData={this.changeData}
-              field={this.preReqs.sixth.field}
-              placeHolder={this.preReqs.sixth.placeHolder}
-              validationType="month"
-              data={this.props.data}
-              topic={this.props.topic}
-              id={this.id}
-            />
-          </div>
-          {this.props.topic === "educate" ? (
-            <div
-              className={`educate-desc-container ${
-                this.state.isDescOpen ? "open" : "close"
-              }`}
-            >
-              <div className="educate-desc-title">
-                <span onClick={this.toggleDescSection}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="12"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                  </svg>
-                  ADD A DESCRIPTION TO YOUR EDUCATION{" "}
-                </span>
-                <div className="educate-desc-tip">
-                  <div className="desc-tip-head">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z" />
-                    </svg>
-                    You can highlight relevant experiences of your academic
-                    career (courses, grades, awards) or write a short paragraph
-                    on your achievements or extracurricular activities.
-                  </div>
-                  <ul>
-                    <li>
-                      If you are a recent graduate, this is an excellent way to
-                      showcase your strengths
-                    </li>
-                    <li>
-                      If you already have the experience, you should only add
-                      essential information
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <EducateTextEditorCont
+          <div
+            className={`others-body body ${
+              this.props.topic === "work" ? "" : "educate-body"
+            }`}
+          >
+            <div className="others-form form">
+              <TextInput
+                className="half"
+                label={this.preReqs.first.label}
                 changeData={this.changeData}
-                placeHolder="(Optional) Type in your educational details."
-                data={this.state[this.props.topic][this.id].description}
+                field={this.preReqs.first.field}
+                placeHolder={this.preReqs.first.placeHolder}
+                validationType="text"
+                data={this.props.data}
+                topic={this.props.topic}
+                id={this.id}
+                isRed={
+                  this.state[this.props.topic][this.id][
+                    this.preReqs.first.field
+                  ] === "" && this.state.showVitalInputs
+                    ? true
+                    : false
+                }
+              />
+              <TextInput
+                className="half"
+                label={this.preReqs.second.label}
+                changeData={this.changeData}
+                field={this.preReqs.second.field}
+                placeHolder={this.preReqs.second.placeHolder}
+                validationType="text"
+                data={this.props.data}
+                topic={this.props.topic}
+                id={this.id}
+                isRed={
+                  this.state[this.props.topic][this.id][
+                    this.preReqs.second.field
+                  ] === "" && this.state.showVitalInputs
+                    ? true
+                    : false
+                }
+              />
+              <TextInput
+                className="half"
+                label={this.preReqs.third.label}
+                changeData={this.changeData}
+                field={this.preReqs.third.field}
+                placeHolder={this.preReqs.third.placeHolder}
+                validationType="text"
+                data={this.props.data}
+                topic={this.props.topic}
+                id={this.id}
+              />
+              <TextInput
+                className="half"
+                label={this.preReqs.forth.label}
+                changeData={this.changeData}
+                field={this.preReqs.forth.field}
+                placeHolder={this.preReqs.forth.placeHolder}
+                validationType="text"
+                data={this.props.data}
+                topic={this.props.topic}
+                id={this.id}
+              />
+              <DateInput
+                className="half"
+                label={this.preReqs.fifth.label}
+                changeData={this.changeData}
+                field={this.preReqs.fifth.field}
+                placeHolder={this.preReqs.fifth.placeHolder}
+                validationType="month"
+                data={this.props.data}
+                topic={this.props.topic}
+                id={this.id}
+              />
+              <DateInput
+                className="half"
+                label={this.preReqs.sixth.label}
+                changeData={this.changeData}
+                field={this.preReqs.sixth.field}
+                placeHolder={this.preReqs.sixth.placeHolder}
+                validationType="month"
+                data={this.props.data}
                 topic={this.props.topic}
                 id={this.id}
               />
             </div>
-          ) : null}
-        </div>
-        <div className="contact-foot foot">
-          <NavigationButtons
-            topic={this.props.topic}
-            data={this.state[this.props.topic]}
-            editData={this.props.changeState}
-            page={this.props.page}
-            currentPageNode={this.props.currentPageNode}
-            completedTopics={this.props.completedTopics}
-            hasNext={this.props.currentPageNode.next === null ? false : true}
-            hasBack={this.props.currentPageNode.back === null ? false : true}
-            toggleAlertBox={this.toggleAlertBox}
-            originalData={this.props.data}
-            filledVitalInputs={this.state.filledVitalInputs}
-            showVitalInputs={this.showVitalInputs}
-          />
+            {this.props.topic === "educate" ? (
+              <div
+                className={`educate-desc-container ${
+                  this.state.isDescOpen ? "open" : "close"
+                }`}
+              >
+                <div className="educate-desc-title">
+                  <span onClick={this.toggleDescSection}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="12"
+                      height="12"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                    </svg>
+                    ADD A DESCRIPTION TO YOUR EDUCATION{" "}
+                  </span>
+                  <div className="educate-desc-tip">
+                    <div className="desc-tip-head">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z" />
+                      </svg>
+                      You can highlight relevant experiences of your academic
+                      career (courses, grades, awards) or write a short
+                      paragraph on your achievements or extracurricular
+                      activities.
+                    </div>
+                    <ul>
+                      <li>
+                        If you are a recent graduate, this is an excellent way
+                        to showcase your strengths
+                      </li>
+                      <li>
+                        If you already have the experience, you should only add
+                        essential information
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <EducateTextEditorCont
+                  changeData={this.changeData}
+                  placeHolder="(Optional) Type in your educational details."
+                  data={this.state[this.props.topic][this.id].description}
+                  topic={this.props.topic}
+                  id={this.id}
+                />
+              </div>
+            ) : null}
+          </div>
+          <div className="contact-foot foot">
+            <NavigationButtons
+              topic={this.props.topic}
+              data={this.state[this.props.topic]}
+              editData={this.props.changeState}
+              page={this.props.page}
+              currentPageNode={this.props.currentPageNode}
+              completedTopics={this.props.completedTopics}
+              hasNext={this.props.currentPageNode.next === null ? false : true}
+              hasBack={this.props.currentPageNode.back === null ? false : true}
+              toggleAlertBox={this.toggleAlertBox}
+              originalData={this.props.data}
+              filledVitalInputs={this.state.filledVitalInputs}
+              showVitalInputs={this.showVitalInputs}
+            />
+          </div>
         </div>
         {this.state.showAlertBox ? (
           <AlertBox
