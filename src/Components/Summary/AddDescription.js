0bd4dc9a6de5fc331ab3dee55f805fda7266205c
Reza <reza1377maleki@gmail.com>
2023-02-06 12:52:20 +0330

    fix add-description-button in educate-summary-page

--- src/Components/Summary/AddDescription.js
+++ src/Components/Summary/AddDescription.js
@@ -3,15 +3,22 @@ import { node } from "@reza2022/linked-list";
 
 class AddDesc extends React.Component {
   saveData = () => {
-    const descPageNode = node(
-      { topic: this.props.topic, page: "description", id: this.props.id },
-      this.props.currentPageNode,
-      this.props.currentPageNode
-    );
+    const descPageNode =
+      this.props.topic === "work"
+        ? node(
+            { topic: this.props.topic, page: "description", id: this.props.id },
+            this.props.currentPageNode,
+            this.props.currentPageNode
+          )
+        : node(
+            { topic: this.props.topic, page: "main", id: this.props.id },
+            this.props.currentPageNode,
+            this.props.currentPageNode
+          );
     this.props.changeState(
       this.props.data,
       this.props.topic,
-      "description",
+      descPageNode.value().page,
       this.props.currentPageNode,
       this.props.completedTopics,
       descPageNode
