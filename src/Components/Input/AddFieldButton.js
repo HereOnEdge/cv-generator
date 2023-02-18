6abb24d3c42a276bc05ac40e7c2868258406a010
Reza <reza1377maleki@gmail.com>
2023-02-18 17:01:21 +0330

    add svg for plus-sign isnide contact-page and resize it so it can be in center of its div

--- src/Components/Input/AddFieldButton.js
+++ src/Components/Input/AddFieldButton.js
@@ -26,7 +26,25 @@ class AddFieldButton extends React.Component {
         onClick={() => this.addField()}
         title={`Add ${this.name}'s input field`}
       >
-        <span className="plus-sign">+</span>
+        <div className="plus-sign">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+            id="IconChangeColor"
+          >
+            {" "}
+            <path
+              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+              id="mainIconPathAttribute"
+              stroke-width="1.5"
+              stroke="currentColor"
+              fill="currentColor"
+            ></path>{" "}
+          </svg>
+        </div>
         {this.capitalizeFirstLetter(this.name)}
       </div>
     );
