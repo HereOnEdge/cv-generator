b83689137b36b87bdac94a95be354ed36de1e727
Reza <reza1377maleki@gmail.com>
2023-02-26 20:51:06 +0330

    add responsive design for intro-pages

--- src/Components/NavigationButtons/BackButton.js
+++ src/Components/NavigationButtons/BackButton.js
@@ -48,7 +48,7 @@ class BackButton extends React.Component {
         >
           <path d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
         </svg>
-        {this.buttonName}
+        <span className="button-name">{this.buttonName}</span>
       </button>
     ) : (
       <div></div>
