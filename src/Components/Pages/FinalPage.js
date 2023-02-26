919f23a80bd80d96535359c1beeb970cdecb6895
Reza <reza1377maleki@gmail.com>
2023-02-26 13:56:36 +0330

    move save-options place

--- src/Components/Pages/FinalPage.js
+++ src/Components/Pages/FinalPage.js
@@ -63,32 +63,6 @@ class FinalPage extends React.Component {
           <p>You can change template style and many other things:)</p>
         </div>
         <div className="final-body body">
-          <div className="save-options">
-            <div
-              className="savePdf-container"
-              title="Download as PDF file"
-              onClick={() => {
-                html2pdf(this.pdfRef.current.children[1], {
-                  filename: this.state.pdfFileName,
-                  html2canvas: {
-                    height: this.state.pdfHeight,
-                    windowHeight: this.state.pdfHeight,
-                  },
-                  jsPDF: { format: this.state.pdfFormat },
-                });
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0z" />
-              </svg>
-            </div>
-          </div>
           <div ref={this.pdfRef} className="main-preview-container">
             <div className="pdf-edit-section">
               <FileNameChanger
@@ -99,6 +73,32 @@ class FinalPage extends React.Component {
                 changeFormat={this.changePdfFormat}
                 activeFormat={this.state.pdfFormat}
               />
+            </div>
+            <div className="save-options">
+              <div
+                className="savePdf-container"
+                title="Download as PDF file"
+                onClick={() => {
+                  html2pdf(this.pdfRef.current.children[1], {
+                    filename: this.state.pdfFileName,
+                    html2canvas: {
+                      height: this.state.pdfHeight,
+                      windowHeight: this.state.pdfHeight,
+                    },
+                    jsPDF: { format: this.state.pdfFormat },
+                  });
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-1 4v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 11.293V7.5a.5.5 0 0 1 1 0z" />
+                </svg>
+              </div>
             </div>
             <Preview data={this.props.data} cvDesign={this.props.cvDesign} />
           </div>
