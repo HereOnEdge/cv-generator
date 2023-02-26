75bbd28ed205401dc60d701d73f425923243543e
Reza <reza1377maleki@gmail.com>
2023-02-26 12:55:25 +0330

    finish page-size-changer css

--- src/Components/FinalPageComponents/PdfSizeChanger.js
+++ src/Components/FinalPageComponents/PdfSizeChanger.js
@@ -19,7 +19,7 @@ export default class PdfSizeChanger extends React.Component {
     return (
       <span className="paper-size" title="Edit Page Size">
         Paper size -{" "}
-        <span>
+        <span className={`${this.state.isEditFormatOpen ? "open" : ""}`}>
           <span className="bold" onClick={this.toggleEditPdfFormat}>
             {this.props.activeFormat}{" "}
             <svg
@@ -27,6 +27,7 @@ export default class PdfSizeChanger extends React.Component {
               width="12"
               height="12"
               viewBox="0 0 16 16"
+              fill="currentColor"
               className={`${this.state.isEditFormatOpen ? "open" : ""}`}
             >
               <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
